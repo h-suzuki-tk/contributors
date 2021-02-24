443026fc17ea0a09570e4636e7f4a111900201de
h.suzuki <hina.suzuki.298@gmail.com>
2021-02-24 19:33:29 +0900

    枠組みの作成

---
+++ app/src/main/java/com/example/contributors/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.contributors
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val ft = supportFragmentManager.beginTransaction().apply {
+            replace(R.id.content, ContributorsListFragment())
+            addToBackStack(null)
+        }
+        ft.commit()
+    }
+
+    public fun replaceFragment(fragment : Fragment) {
+        val ft = supportFragmentManager.beginTransaction().apply {
+            replace(R.id.content, fragment)
+            addToBackStack(null)
+        }
+        ft.commit()
+    }
+
+}
