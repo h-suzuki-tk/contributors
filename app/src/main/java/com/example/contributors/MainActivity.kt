5ca516681454f547d55b1857a6c80d9fc47901e8
h.suzuki <hina.suzuki.298@gmail.com>
2021-02-26 01:54:50 +0900

    ツールバーのセット

--- app/src/main/java/com/example/contributors/MainActivity.kt
+++ app/src/main/java/com/example/contributors/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.contributors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.tool_bar)
+        toolbar.apply {
+            title = ""
+        }
+        setSupportActionBar(toolbar)
 
         replaceFragment(ContributorsListFragment())
     }
